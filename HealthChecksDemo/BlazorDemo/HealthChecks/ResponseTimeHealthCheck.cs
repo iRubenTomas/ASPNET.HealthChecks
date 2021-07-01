e943a131bcfbcdc857b98df4a7eae8c12fff8343
Ruben Tomas <irubentomas@gmail.com>
2021-07-01 21:48:19 +0100

    Demo added

---
+++ HealthChecksDemo/BlazorDemo/HealthChecks/ResponseTimeHealthCheck.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlazorDemo.HealthChecks
+{
+    public class ResponseTimeHealthCheck : IHealthCheck
+    {
+        private Random rnd = new Random();
+        
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            //THIS IS HARDCODED - for a real world application we should do a ping or other types of mechanism in order to get the real value of response time
+            int responseTime = rnd.Next(1, 300);
+
+            if(responseTime < 100)
+            {
+                return Task.FromResult(
+                    HealthCheckResult.Healthy($"The response time looks good ({responseTime})."));
+            }
+            else if( responseTime < 200)
+            {
+                return Task.FromResult(
+                    HealthCheckResult.Healthy($"The response time is a bit slow ({responseTime})."));
+            }
+            else
+            {
+                return Task.FromResult(
+                    HealthCheckResult.Unhealthy($"The response time is unacceptable ({responseTime})."));
+            }
+        }
+    }
+
+    public static class HelperMethods
+    {
+        public static Random rnd = new Random();
+    }
+}
---
+++ HealthChecksDemo/BlazorDemo/Startup.cs
@@ -0,0 +1,97 @@
+using BlazorDemo.Data;
+using BlazorDemo.HealthChecks;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+
+            //Configure Health Checks
+            
+            services.AddHealthChecks() 
+                //.AddCheck("Foo Service", () =>
+                //    HealthCheckResult.Degraded("The check of the foo service did not work well"), new[] { "service" })
+                //.AddCheck("Bar Service", () =>
+                //    HealthCheckResult.Healthy("The check of the bar service worked"), new[] { "service"})
+                .AddCheck<ResponseTimeHealthCheck>("Network speed test", null, new [] { "service" })
+                .AddCheck("Database Service", () =>
+                    HealthCheckResult.Healthy("The check of the database"), new[] { "database", "sql" });
+
+
+
+
+            services.AddSingleton<WeatherForecastService>();
+            // "Singleton" - We get one instance and will use it for the life of application which allows to keep state between our calls
+            services.AddSingleton<ResponseTimeHealthCheck>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHealthChecks("/quickhealth", new HealthCheckOptions()
+                {
+                    Predicate = _ => false
+                });
+
+                endpoints.MapHealthChecks("/health", new HealthCheckOptions()
+                {
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
+
+                endpoints.MapHealthChecks("/health/services", new HealthCheckOptions()
+                {
+                    Predicate = reg => reg.Tags.Contains("service"),
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
+
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
